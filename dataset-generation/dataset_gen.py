9d08678c3307ae4b49981a950dca5dbd049a72e1
Thomas Metcalfe <tommetcalfe@live.com>
2024-03-15 12:40:25 +0100

    Fix error when SCP solution is infeasible

--- dataset-generation/dataset_gen.py
+++ dataset-generation/dataset_gen.py
@@ -68,10 +68,10 @@ for i in range(N_data):
     if np.char.equal(feas_cvx_i,'optimal'):
 
         #  Solve transfer scp
-        states_roe_scp_i, actions_scp_i, feas_scp_i = solve_scp(stm_i, cim_i, psi_i, state_roe_0, states_roe_cvx_i, n_time_rpod)
-        states_rtn_scp_i = roe_to_rtn_horizon(states_roe_scp_i, oe_i, n_time_rpod)
+        states_roe_scp_i, actions_scp_i, feas_scp_i, _, _, _ = solve_scp(stm_i, cim_i, psi_i, state_roe_0, states_roe_cvx_i, n_time_rpod)
 
         if np.char.equal(feas_scp_i,'optimal'):
+            states_rtn_scp_i = roe_to_rtn_horizon(states_roe_scp_i, oe_i, n_time_rpod)
             states_roe_cvx[i,:,:] = np.transpose(states_roe_cvx_i)
             states_rtn_cvx[i,:,:] = np.transpose(states_rtn_cvx_i)
             actions_cvx[i,:,:] = np.transpose(actions_cvx_i)
